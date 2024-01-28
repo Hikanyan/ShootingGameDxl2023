3c4d3861add3c87c62a9f17b652243ace90232af
KoiiroHikanyan <hikanyan0131@gmail.com>
2024-01-28 12:48:03 +0900

    [Update]

--- ManagedDxlGame/program/game/hikanyan_laboratory/Entity/Entity.h
+++ ManagedDxlGame/program/game/hikanyan_laboratory/Entity/Entity.h
@@ -1,7 +1,13 @@
 ﻿#pragma once
+#include "../hikanyan_library/include/mono_behaviour/MonoBehaviour.h"
 
-class Entity
+class Entity : MonoBehaviour
 {
 public:
+    int id; // エンティティの一意のID
+    std::string tag; // エンティティのカテゴリーを示すタグ
+    bool is_active; // エンティティがアクティブかどうか
+    int version; // ID再利用時のバージョン管理用
+
     
 };
